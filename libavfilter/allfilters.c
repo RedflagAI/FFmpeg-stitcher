7af75b8c015f440943b34d7e1f0f97396dd1d2a5
Thomas Ulmer <thomasmulmer02@gmail.com>
2025-04-14 11:37:05 -0700

    staging playground

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -385,6 +385,7 @@ extern const AVFilter ff_vf_ocr;
 extern const AVFilter ff_vf_ocv;
 extern const AVFilter ff_vf_oscilloscope;
 extern const AVFilter ff_vf_overlay;
+extern const AVFilter ff_vf_stitcher;
 extern const AVFilter ff_vf_overlay_opencl;
 extern const AVFilter ff_vf_overlay_qsv;
 extern const AVFilter ff_vf_overlay_vaapi;
